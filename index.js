ce6498f374d1dfb6b698948edf4b8b380b839b30
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-04 15:21:36 +0100

    Completed HW

---
+++ index.js
@@ -0,0 +1,139 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+*/
+
+
+/* EXERCISE 1
+Create and array containing the first 5 positive numbers.
+*/
+
+let numbers = [1, 2, 3, 4, 5]
+
+/* EXERCISE 2
+Create an object containing your name, surname, email address and age.
+*/
+
+let me = {
+    name: "Wako",
+    surname: "Anindo",
+    email: "wakoanindo@outlook.com",
+    age: 29
+}
+
+/* EXERCISE 3
+Add to the previously created object a boolean value to represent whether you have or not a driving license.
+*/
+
+me.drivingLisence = false
+
+/* EXERCISE 4
+Remove from the previously created object the age property.
+*/
+
+delete me.age
+
+/* EXERCISE 5
+Create a second object with name, surname, email address and verify that this object has a different email address than the previous one.
+*/
+
+secondObject = {
+    name: "Naruto",
+    surname: "Uzumaki",
+    email: "hokage@konohamail.com",
+}
+console.log("The email is equal to the previous one: ", me.email !== secondObject.email)
+
+/* EXERCISE 6
+You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
+Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
+Write an algorithm that calculates the total cost to charge the user with.
+*/
+
+let totalShoppingCart = 100
+if (totalShoppingCart <= 50){
+    totalShoppingCart += 10 // totalShoppingCart = totalShoppingCart + 10
+}
+    
+/* EXERCISE 7
+You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
+Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
+*/
+
+let discount = totalShoppingCart * 0.2
+let BlackFridayShoppingCart = totalShoppingCart - discount
+
+/* EXERCISE 8
+Create an object representing a car with properties like brand, model, licensePlate.
+After you create the first car, clone it 5 times and change the licensePlate for each cloned car without affecting the original one.
+*/
+
+let car = {
+    brand: "Tesla",
+    model: "Model X",
+    licensePlate: "HAWEHFOIOIHO0869",
+}
+
+let car2 = { }
+Object.assign(car2, car)
+car2.licensePlate = "HAWEHFOIOIHO0887"
+
+let car3 = { }
+Object.assign(car3, car)
+car3.licensePlate = "HAWEHFOIOIHO0881"
+
+let car4 = { }
+Object.assign(car4, car)
+car4.licensePlate = "HAWEHFOIOIHO0841"
+
+let car5 = { }
+Object.assign(car5, car)
+car5.licensePlate = "HAWEHFOIOIHO0851"
+
+let car6 = { }
+Object.assign(car3, car)
+car6.licensePlate = "HAWEHFOIOIHO1881"
+
+/* EXERCISE 9
+Create a new array called carsForRent containing all the cars from the previous exercise.
+*/
+
+let carsForRent = [car, car2, car3, car4, car5, car6]
+
+/* EXERCISE 10
+Remove the first and the last car from the carsForRent array.
+*/
+
+carsForRent.pop()
+carsForRent.shift()
+
+console.log(carsForRent)
+
+/* EXERCISE 11
+Print in the console the TYPES of the car variable, of its licensePlate and of the its brand properties.
+*/
+
+console.log(car.brand,car.model, car.licensePlate)
+
+/* EXERCISE 12
+Create a new array called carsForSale and insert 3 cars in it.
+Store in a variable totalCars the number of cars present in both carsForSale and carsForRent arrays.
+*/
+
+carsForSale = [car4, car3, car1]
+let totalCars = {
+    carsForRent: carsForRent.length,
+    carsForSale: carsForSale.length,
+}
+
+/* EXERCISE 13
+Print in the console the data from each car in the carsForSale array.
+*/
+console.log(carsForSale[0])
+console.log(carsForSale[1])
+console.log(carsForSale(2))
